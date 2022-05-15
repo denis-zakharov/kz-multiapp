238ed68c4f11e6d217a3b53b116e796694bcc2b1
Denis Zakharov <dizaharov@gmail.com>
2022-05-15 18:05:41 +0300

    inject Java Opts

--- java-opts-transformer/pkg/config.go
+++ java-opts-transformer/pkg/config.go
@@ -37,9 +37,17 @@ func (trans *JavaOptsTransformerv1alpha1) Validate() error {
 	return nil
 }
 
+func isDeployment(rn *yaml.RNode) bool {
+	kind := rn.GetKind()
+	return kind == "Deployment" || kind == "StatefulSet"
+}
+
 func (trans *JavaOptsTransformerv1alpha1) Filter(items []*yaml.RNode) ([]*yaml.RNode, error) {
-	for i := range items {
-		err := items[i].PipeE(yaml.SetAnnotation("custom.io/the-value", trans.Metadata.Name))
+	for _, item := range items {
+		if !isDeployment(item) || item.GetName() != trans.Metadata.Name {
+			continue
+		}
+		err := item.PipeE(injectJavaOpts(trans.Spec))
 		if err != nil {
 			return nil, err
 		}
@@ -52,4 +60,79 @@ func (trans *JavaOptsTransformerv1alpha1) BuildProcessor() framework.ResourceLis
 		Config: trans,
 		Filter: trans,
 	}
+}
+
+type JavaOptsSetter struct {
+	MemLimit string
+	Value    string
+	Index    int
+}
+
+func injectJavaOpts(spec Spec) JavaOptsSetter {
+	memLimitNum := int(float64(spec.Heap) / spec.Ratio)
+	memLimit := fmt.Sprintf("%dMi", memLimitNum)
+	jvmOptsValue := fmt.Sprintf("-Xms%dm -Xmx%dm %s", spec.Heap, spec.Heap, spec.Extra)
+	return JavaOptsSetter{memLimit, jvmOptsValue, spec.Index}
+}
+
+func (s JavaOptsSetter) Filter(item *yaml.RNode) (*yaml.RNode, error) {
+	jvmOpts, err := yaml.Parse(`
+name: JAVA_OPTIONS
+value: ` + s.Value)
+	if err != nil {
+		return item, err
+	}
+
+	// remove existing JAVA_OPTIONS var
+	err = item.PipeE(
+		yaml.LookupCreate(yaml.SequenceNode,
+			"spec", "template", "spec", "containers"),
+		yaml.GetElementByIndex(s.Index),
+		yaml.LookupCreate(yaml.SequenceNode, "env"),
+		yaml.ElementSetter{
+			Keys:    []string{"name"},
+			Values:  []string{"JAVA_OPTIONS"},
+			Element: nil,
+		},
+	)
+	if err != nil {
+		return item, err
+	}
+	// append JAVA_OPTIONS to the env spec end
+	err = item.PipeE(
+		yaml.LookupCreate(yaml.SequenceNode,
+			"spec", "template", "spec", "containers"),
+		yaml.GetElementByIndex(s.Index),
+		yaml.LookupCreate(yaml.SequenceNode, "env"),
+		yaml.ElementAppender{Elements: []*yaml.Node{jvmOpts.YNode()}},
+	)
+	if err != nil {
+		return item, err
+	}
+
+	// requests
+	err = item.PipeE(
+		yaml.LookupCreate(yaml.SequenceNode,
+			"spec", "template", "spec", "containers"),
+		yaml.GetElementByIndex(s.Index),
+		yaml.LookupCreate(yaml.MappingNode, "resources", "requests"),
+		yaml.SetField("memory", yaml.NewStringRNode(s.MemLimit)),
+	)
+	if err != nil {
+		return item, err
+	}
+
+	// limits
+	err = item.PipeE(
+		yaml.LookupCreate(yaml.SequenceNode,
+			"spec", "template", "spec", "containers"),
+		yaml.GetElementByIndex(s.Index),
+		yaml.LookupCreate(yaml.MappingNode, "resources", "limits"),
+		yaml.SetField("memory", yaml.NewStringRNode(s.MemLimit)),
+	)
+	if err != nil {
+		return item, err
+	}
+
+	return item, nil
 }
