3defda815625d8bc838b7ab0cb2b8f294b1122f5
Denis Zakharov <dizaharov@gmail.com>
2022-05-15 12:55:26 +0300

    kyaml experiments

---
+++ kyaml-experiments/go.mod
@@ -0,0 +1,3 @@
+module github.com/denis-zakharov/kz-multiapp/kyaml-experiments
+
+go 1.18
---
+++ kyaml-experiments/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadYAML(path string) *yaml.RNode {
+	f, err := os.Open(path)
+	must(err)
+	b, err := io.ReadAll(f)
+	must(err)
+	return parseYAML(string(b))
+}
+
+func parseYAML(data string) *yaml.RNode {
+	r, err := yaml.Parse(data)
+	must(err)
+	return r
+}
+
+func processYAML(r *yaml.RNode) {
+
+	jvmOpts := parseYAML(`
+name: JAVA_OPTIONS
+value: SUPER_HEAP
+`)
+
+	// remove
+	err := r.PipeE(
+		yaml.LookupCreate(yaml.SequenceNode,
+			"spec", "template", "spec", "containers", "[name=java]", "env"),
+		yaml.ElementSetter{
+			Keys:    []string{"name"},
+			Values:  []string{"JAVA_OPTIONS"},
+			Element: nil,
+		},
+	)
+	must(err)
+	// append
+	err = r.PipeE(
+		yaml.LookupCreate(yaml.SequenceNode,
+			"spec", "template", "spec", "containers", "[name=java]", "env"),
+		yaml.ElementAppender{Elements: []*yaml.Node{jvmOpts.YNode()}},
+	)
+	must(err)
+
+	fmt.Println(r.String())
+}
+
+func main() {
+	for _, v := range os.Args[1:] {
+		r := loadYAML(v)
+		processYAML(r)
+	}
+}
